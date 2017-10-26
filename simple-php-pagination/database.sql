e86a4325226d1a1f98bff3a872212a7865be29b8
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2017-10-26 12:10:02 +0530

    added simple php pagination script

---
+++ simple-php-pagination/database.sql
@@ -0,0 +1,38 @@
+use database `countries`;
+
+create table `countries` (
+	`id` int ,
+	`sortname` varchar ,
+	`name` varchar 
+); 
+
+insert into `countries` (`id`, `sortname`, `name`) values('1','AF','Afghanistan');
+insert into `countries` (`id`, `sortname`, `name`) values('2','AL','Albania');
+insert into `countries` (`id`, `sortname`, `name`) values('3','DZ','Algeria');
+insert into `countries` (`id`, `sortname`, `name`) values('4','AS','American Samoa');
+insert into `countries` (`id`, `sortname`, `name`) values('5','AD','Andorra');
+insert into `countries` (`id`, `sortname`, `name`) values('6','AO','Angola');
+insert into `countries` (`id`, `sortname`, `name`) values('7','AI','Anguilla');
+insert into `countries` (`id`, `sortname`, `name`) values('8','AQ','Antarctica');
+insert into `countries` (`id`, `sortname`, `name`) values('9','AG','Antigua And Barbuda');
+insert into `countries` (`id`, `sortname`, `name`) values('10','AR','Argentina');
+insert into `countries` (`id`, `sortname`, `name`) values('11','AM','Armenia');
+insert into `countries` (`id`, `sortname`, `name`) values('12','AW','Aruba');
+insert into `countries` (`id`, `sortname`, `name`) values('13','AU','Australia');
+insert into `countries` (`id`, `sortname`, `name`) values('14','AT','Austria');
+insert into `countries` (`id`, `sortname`, `name`) values('15','AZ','Azerbaijan');
+insert into `countries` (`id`, `sortname`, `name`) values('16','BS','Bahamas The');
+insert into `countries` (`id`, `sortname`, `name`) values('17','BH','Bahrain');
+insert into `countries` (`id`, `sortname`, `name`) values('18','BD','Bangladesh');
+insert into `countries` (`id`, `sortname`, `name`) values('19','BB','Barbados');
+insert into `countries` (`id`, `sortname`, `name`) values('20','BY','Belarus');
+insert into `countries` (`id`, `sortname`, `name`) values('21','BE','Belgium');
+insert into `countries` (`id`, `sortname`, `name`) values('22','BZ','Belize');
+insert into `countries` (`id`, `sortname`, `name`) values('23','BJ','Benin');
+insert into `countries` (`id`, `sortname`, `name`) values('24','BM','Bermuda');
+insert into `countries` (`id`, `sortname`, `name`) values('25','BT','Bhutan');
+insert into `countries` (`id`, `sortname`, `name`) values('26','BO','Bolivia');
+insert into `countries` (`id`, `sortname`, `name`) values('27','BA','Bosnia and Herzegovina');
+insert into `countries` (`id`, `sortname`, `name`) values('28','BW','Botswana');
+insert into `countries` (`id`, `sortname`, `name`) values('29','BV','Bouvet Island');
+insert into `countries` (`id`, `sortname`, `name`) values('30','BR','Brazil');
